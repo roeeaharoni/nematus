0163a85299c3b35d8995e7bb8b90e76a5af95814
roeeaharoni <roee.aharoni@gmail.com>
2017-06-22 15:26:09 +0300

    supress inputs-2 after cast to int

--- nematus/nmt.py
+++ nematus/nmt.py
@@ -661,8 +661,7 @@ def gen_sample(f_init, f_next, x, trng=None, k=1, maxlen=30,
                stochastic=True, argmax=False, return_alignment=False, suppress_unk=False,
                return_hyp_graph=False):
     print 'input word ids:'
-    print x
-    print 'input dim is {}'.format(x.shape)
+    print x.flatten()
 
     # k is the beam size we have
     if k > 1 and argmax:
